21367cd57c16e3ddc85fd7c791e8458bb57fd301
Augusto Seta <alseta97@gmail.com>
2023-03-05 02:00:21 -0300

    StyledComponents File Created
    File that imports components styled with styled-components.

---
+++ src/components/QrContainer/StyledComponents.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const StyledQrContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 12px;
+  width: 280px;
+  padding: 13px;
+  padding-bottom: 30px;
+  background-color: hsl(0, 0%, 100%);
+`
+
+export const StyledImage = styled.img`
+  max-width: 280px;
+  border-radius: 12px;
+`
+
+export const StyledH1 = styled.h1`
+  text-align: center;
+  padding: 0 8px;
+  color: hsl(218, 44%, 22%);
+  margin: 15px 0;
+  font-weight: 700;
+  font-size: 21px;
+`
+
+export const StyledP = styled.p`
+  margin: 0;
+  padding: 0 8px;
+  color: hsl(220, 15%, 55%);
+  text-align: center;
+  font-size: 15px;
+`
