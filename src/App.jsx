b72aa6145ef7fb4c2772924feca10bbf3b5d9a76
Augusto Seta <alseta97@gmail.com>
2023-03-05 02:08:00 -0300

    App Component Created
    Component that renders the application in the DOM and makes use of framer-motion to display a transition at startup.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { easeOut, motion } from 'framer-motion'
+import QrContainer from './components/QrContainer/QrContainer'
+
+import { GlobalStyles } from './styles/GlobalStyles';
+
+function App() {
+
+  return (
+    <>
+      <motion.div 
+        className='App'
+        initial={{ y: -600, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeOut", duration: 2 }}
+      >
+        <QrContainer />
+      </motion.div>
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
